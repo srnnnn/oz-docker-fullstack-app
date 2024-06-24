db59a502d7edac1dc004709db417d54cb5d0219a
srnnnn <tpfla311@naver.com>
2024-06-24 17:10:13 +0900

    commit3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import axios from "axios";
-const axios = require("axios");
+import axios from "axios";
+// const axios = require("axios").default;
 
 function App() {
   const [lists, setLists] = useState([]);
